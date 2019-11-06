10464463764df049b246e71cd3134b8b88310c2a
Axel Pantoja <axelpantoja08@gmail.com>
2019-11-06 15:38:21 -0600

    App terminada.

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,29 @@ class Estacionamiento extends StatefulWidget {
 }
 class EstadoEstacionamiento extends State {
 
+  //Horas:
   String entrada = "--:--:--";
   String salida = "--:--:--";
+
+  //Costos:
+  int costoHora = 30;
+  int costoFraccion = 15;
+
+  //Totales:
+  int horasTotales = 0;
+  int minutosTotales = 0;
+  int segundosTotales = 0;
+  String tiempoTotal = "--:--:--";
+  int costoTotal = 0;
+
+  void snackBar(BuildContext context) {
+    final scaffold = Scaffold.of(context);
+    scaffold.showSnackBar(
+      SnackBar(
+        content: const Text("No puedes viajar en el tiempo!!!"),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,6 +42,7 @@ class EstadoEstacionamiento extends State {
       home: Scaffold(
         appBar: AppBar(
           title: Text("Estacionamiento"),
+          backgroundColor: Colors.teal,
         ),
         body:  Builder( //El contexto no servía para el timePicker.
           builder: (context) => Padding( //Se usa Builder para tener otro contexto que si se pueda usar.
@@ -58,6 +80,45 @@ class EstadoEstacionamiento extends State {
                           (hora.second.toString().length == 1) ? segundos = "0${hora.second}" : segundos = "${hora.second}";
 
                           entrada = "$horas:$minutos:$segundos";
+
+                          if (salida != "--:--:--") {
+                            horasTotales = int.parse(salida.substring(0, 1)) - int.parse(entrada.substring(0, 1));
+                            minutosTotales = int.parse(salida.substring(3, 4)) - int.parse(entrada.substring(3, 4));
+                            segundosTotales = int.parse(salida.substring(6, 7)) - int.parse(entrada.substring(6, 7));
+
+                            if (minutosTotales < 0) {
+                              horasTotales--;
+                              minutosTotales = 60 + minutosTotales;
+                            }
+                            if (segundosTotales < 0) {
+                              if (minutosTotales == 0) {
+                                horasTotales--;
+                                minutosTotales = 60;
+                              }
+                              minutosTotales--;
+                              segundosTotales = 60 + segundosTotales;
+                            }
+
+                            String h, m, s;
+                            (horasTotales.toString().length == 1) ? h = "0${horasTotales.toString()}" : h = "${horasTotales.toString()}";
+                            (minutosTotales.toString().length == 1) ? m = "0${minutosTotales.toString()}" : m = "${minutosTotales.toString()}";
+                            (segundosTotales.toString().length == 1) ? s = "0${segundosTotales.toString()}" : s = "${segundosTotales.toString()}";
+
+                            if (horasTotales < 0 || minutosTotales < 0 || segundosTotales <0) {
+                              snackBar(context);
+                              tiempoTotal = "--:--:--";
+                              costoTotal = 0;
+                            } else {
+                              tiempoTotal = "$h:$m:$s";
+
+                              if (horasTotales > 0) {
+                                costoTotal = horasTotales * 30;
+                              }
+                              if (minutosTotales > 0 || segundosTotales > 0) {
+                                costoTotal += 15;
+                              }
+                            }
+                          }
                         });
                       },
                       currentTime: DateTime.now(),
@@ -134,6 +195,45 @@ class EstadoEstacionamiento extends State {
                           (hora.second.toString().length == 1) ? segundos = "0${hora.second}" : segundos = "${hora.second}";
 
                           salida = "$horas:$minutos:$segundos";
+
+                          if (entrada != "--:--:--") {
+                            horasTotales = int.parse(horas) - int.parse(entrada.substring(0, 2));
+                            minutosTotales = int.parse(minutos) - int.parse(entrada.substring(3, 5));
+                            segundosTotales = int.parse(segundos) - int.parse(entrada.substring(6, 8));
+
+                            if (minutosTotales < 0) {
+                              horasTotales--;
+                              minutosTotales = 60 + minutosTotales;
+                            }
+                            if (segundosTotales < 0) {
+                              if (minutosTotales == 0) {
+                                horasTotales--;
+                                minutosTotales = 60;
+                              }
+                              minutosTotales--;
+                              segundosTotales = 60 + segundosTotales;
+                            }
+
+                            String h, m, s;
+                            (horasTotales.toString().length == 1) ? h = "0${horasTotales.toString()}" : h = "${horasTotales.toString()}";
+                            (minutosTotales.toString().length == 1) ? m = "0${minutosTotales.toString()}" : m = "${minutosTotales.toString()}";
+                            (segundosTotales.toString().length == 1) ? s = "0${segundosTotales.toString()}" : s = "${segundosTotales.toString()}";
+
+                            if (horasTotales < 0 || minutosTotales < 0 || segundosTotales <0) {
+                              snackBar(context);
+                              tiempoTotal = "--:--:--";
+                              costoTotal = 0;
+                            } else {
+                              tiempoTotal = "$h:$m:$s";
+
+                              if (horasTotales > 0) {
+                                costoTotal = horasTotales * 30;
+                              }
+                              if (minutosTotales > 0 || segundosTotales > 0) {
+                                costoTotal += 15;
+                              }
+                            }
+                          }
                         });
                       },
                       currentTime: DateTime.now(),
@@ -253,7 +353,7 @@ class EstadoEstacionamiento extends State {
                               ),
                             ),
                             Text(
-                              "00:00:00",
+                              "$tiempoTotal",
                               style: TextStyle(
                                   fontSize: 20.0
                               ),
@@ -281,7 +381,7 @@ class EstadoEstacionamiento extends State {
                               ),
                             ),
                             Text(
-                              "\$500",
+                              "\$$costoTotal",
                               style: TextStyle(
                                   fontSize: 20.0,
                                   fontWeight: FontWeight.bold,
